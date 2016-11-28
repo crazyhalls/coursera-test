92740d6f0e3729d1a4ee699e748d6cc71aba96af
Danilo <danilocordeiromorais@hotmail.com>
2016-11-28 18:21:41 -0200

    Implemented project base

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -2,10 +2,18 @@
 'use strict';
 
 angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('LunchCheckController', function ($scope) {
+LunchCheckController.$inject = ['$scope'];
 
-})
+function LunchCheckController($scope) {
+  $scope.lunch = "";
+  var count = 0;
 
+  $scope.checkLunch = function ($scope.lunch) {
+    var lunchItens = $scope.lunch.split(",");
+    console.log(lunchItens);
+  };
+}
 
 })();
